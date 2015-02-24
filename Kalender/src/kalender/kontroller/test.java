189005e194621866f65a576953d05d530e6041c8
ssbolsta <syver92@msn.com>
2015-02-24 12:38:33 +0100

    test->Test

--- Kalender/src/kalender/kontroller/test.java
+++ Kalender/src/kalender/kontroller/test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		UserController uc = new UserController();
-		
+
 //		uc.searchUserByLastName("12Svoren");
 		uc.searchUserByPnr("12345678fdsds");
 
