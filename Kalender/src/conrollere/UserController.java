980a1fb6a35e081a1349fbbc068c453719ebe205
krissvor <krissvor@gmail.com>
2015-02-24 12:00:50 +0100

    Flyttet valideringsmetoder inn i egen klasse

--- Kalender/src/conrollere/UserController.java
+++ Kalender/src/conrollere/UserController.java
@@ -4,7 +4,7 @@ public class UserController {
 
 	public void searchUserByLastName(String lastName){
 		try{
-			validateName(lastName);
+			Validation.validateName(lastName);
 		}
 		catch(Exception e){
 			System.out.println(e);
@@ -12,36 +12,14 @@ public class UserController {
 		}
 	}
 
-	private void validateName(String name) throws Exception{
-		name.trim();
-		char[] chars = name.toCharArray();
-
-		for (char c : chars) {
-			if(!Character.isLetter(c)) {
-				throw new Exception ("Navn kan ikke inneholde tall");
-			}
-		}
-	}
+	
 
 	public void searchUserByPnr(String pnr){
 		try{
-			validatePnr(pnr);
+			Validation.validatePnr(pnr);
 		}
 		catch(Exception e){
 			System.out.println(e);
-		}
-	}
-	
-	public void validatePnr(String pnr)throws Exception{
-		pnr.trim();
-		try{
-			Long.parseLong(pnr);
-		}
-		catch(Exception e){
-			System.out.println("Personnummer må bestå av elleve tall");
-		}
-		if(pnr.length()!= 11){
-			throw new Exception ("Feil lengde");
 		}
 	}
 }
