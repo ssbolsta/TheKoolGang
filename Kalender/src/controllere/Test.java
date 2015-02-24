24f8d1299c89ba7ec6eccec36cdf16617eca8500
krissvor <krissvor@gmail.com>
2015-02-24 12:14:30 +0100

    Laget loginGUI og skiftet navn på controllere

---
+++ Kalender/src/controllere/Test.java
@@ -0,0 +1,13 @@
+package controllere;
+
+public class Test {
+
+	public static void main(String[] args) {
+		UserController uc = new UserController();
+		
+//		uc.searchUserByLastName("12Svoren");
+		uc.searchUserByPnr("12345678fdsds");
+
+	}
+
+}
---
+++ Kalender/src/controllere/UserController.java
@@ -0,0 +1,26 @@
+package controllere;
+
+public class UserController {
+
+	public void searchUserByLastName(String lastName){
+		try{
+			Validation.validateName(lastName);
+		}
+		catch(Exception e){
+			System.out.println(e);
+
+		}
+	}
+
+	
+
+	public void searchUserByPnr(String pnr){
+		try{
+			Validation.validatePnr(pnr);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
+
