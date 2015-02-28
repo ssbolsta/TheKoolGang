f62175ce2150db67bc4721c8d01d094cebef4ca3
mhegedal <mhegedal@stud.ntnu.no>
2015-02-28 15:59:56 +0100

    bare noe pirke greier

--- Kalender/src/controllere/InvitationsController.java
+++ Kalender/src/controllere/InvitationsController.java
@@ -16,6 +16,10 @@ import javafx.scene.text.Text;
 
 public class InvitationsController {
 	
+	@FXML
+	private Text eventLabel;
+	@FXML
+	private Text eventDesc;
 	@FXML 
 	private Text tag;
 	@FXML 
@@ -38,7 +42,7 @@ public class InvitationsController {
 	@FXML
 	private void initialize(){
 		invitationTableList.add(new Invitation(new Person("Mats","Egedal",1234),new Person("Mats","Egedal",1234), new Group(1,"Balletak",new Person("hore","faen",4567))));
-		invitationTableList.add(new Invitation(new Person("Boye","Data",1236),new Person("John","Smith",1237), new Event("Harem",LocalDate.of(2015, 3,25 ),LocalTime.of(12, 0),LocalTime.of(14, 0),"",new Person("hore","faen",4567))));
+		invitationTableList.add(new Invitation(new Person("Boye","Data",1236),new Person("John","Smith",1237), new Event("Harem",LocalDate.of(2015, 3,25 ),LocalTime.of(12, 0),LocalTime.of(14, 0),"Vi skal holde et stort harem på jobben. Bar eå komme, blir masse kos!",new Person("hore","faen",4567))));
 		dateColumn.setCellValueFactory(cellData -> cellData.getValue().getDateProperty());
 		tagColumn.setCellValueFactory(cellData -> cellData.getValue().getTagProperty());
 		timeColumn.setCellValueFactory(cellData -> cellData.getValue().getTimeProperty());
@@ -54,16 +58,20 @@ public class InvitationsController {
 			date.setText(invitation.getDateAsString());
 			time.setText(invitation.getTimeAsString());
 			if(invitation.getGroup() != null){
+				eventLabel.setText("");
 				desc.setText("Du har mottatt en forespørsel om å bli med i gruppen " + invitation.getGroup().getName() + ". Trykk på knappen for å akseptere/avslå.");
 			}else{
+				eventLabel.setText("Arrangement beskrivelse:");
 				desc.setText("Du har blitt invitert til " + invitation.getEvent().getName() + " den " + invitation.getEvent().getDateAsString() + " klokken "
 						+ invitation.getEvent().getTimeFromAsString() + " til " + invitation.getEvent().getTimeToAsString() + ". Trykk på knappen for å akseptere/avslå.");
+				eventDesc.setText(invitation.getEvent().getDescription());
 			}
 		}else{
 			tag.setText("");
 			date.setText("");
 			time.setText("");
 			desc.setText("");
+			eventDesc.setText("");
 		}
 	}
 }
