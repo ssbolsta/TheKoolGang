dfc20a4f615090d05768cac16336279aadb4e7cf
Jo <jaarvaag@hotmail.com>
2015-03-03 15:54:21 +0100

    Pusher delete requests

---
+++ Kalender/src/requests/DeleteEventRequest.java
@@ -0,0 +1,44 @@
+package requests;
+
+import org.json.simple.JSONObject;
+
+
+/*
+ * {
+	"request" : "event",
+	"type" : "delete",
+	"content" : {a
+		"id" : "<eid>"
+	}
+ */
+
+public class DeleteEventRequest {
+
+	private int id = -1;
+
+	public String toString(){
+		
+	
+	JSONObject main = new JSONObject();
+	JSONObject content = new JSONObject();
+
+	content.put("id", id);
+	
+	main.put("request", "event");
+	main.put("type", "delete");
+	main.put("content", content);
+	
+	return main.toJSONString();
+	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
