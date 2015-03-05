3eaa4defbbfc10702eb36da81039afe31a7aa4dd
Boye Borg <boye.borg@gmail.com>
2015-03-05 13:01:11 +0100

    fixed returntype of sendRequest

--- Kalender/src/connection/ServerConnection.java
+++ Kalender/src/connection/ServerConnection.java
@@ -3,68 +3,71 @@ package connection;
 import java.io.*;
 import java.net.Socket;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 import requests.Request;
 
 public class ServerConnection {
-	
-	private BufferedWriter out;
-	private BufferedReader in;
-	private Socket client;
-	
-	private String SERVER_IP = "localhost";
-	private int SERVER_PORT = 5432;
-	
-	public ServerConnection() throws IOException {
-		client = new Socket(SERVER_IP, SERVER_PORT);
-		
-		out = new BufferedWriter (new OutputStreamWriter(client.getOutputStream()));
-		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-	}
+    private BufferedWriter out;
+    private BufferedReader in;
+    private Socket client;
 
-    public ServerConnection(String ip, int port) throws IOException {
-        
-        SERVER_IP = ip;
-        SERVER_PORT = port;
-        
+    private String SERVER_IP = "localhost";
+    private int SERVER_PORT = 5432;
+
+    public ServerConnection() throws IOException {
         client = new Socket(SERVER_IP, SERVER_PORT);
 
         out = new BufferedWriter (new OutputStreamWriter(client.getOutputStream()));
         in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
     }
-	
-	public void close() throws IOException {
-		client.close();
-	}
-	
-	public JSONObject sendRequest(Request request) throws IOException {
+
+    public ServerConnection(String ip, int port) throws IOException {
+
+        SERVER_IP = ip;
+        SERVER_PORT = port;
+
+        client = new Socket(SERVER_IP, SERVER_PORT);
+
+        out = new BufferedWriter (new OutputStreamWriter(client.getOutputStream()));
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+    }
+
+    public void close() throws IOException {
+        client.close();
+    }
+
+    public JSONArray sendRequest(Request request) throws IOException {
 
         out.write(request.toString());
         out.flush();
-        
+
         String result = in.readLine();
-        JSONObject result_json;
-        
+        JSONArray result_json;
+
         try {
-            result_json = (JSONObject)new JSONParser().parse(result);
+            result_json = (JSONArray) new JSONParser().parse(result);
         } catch (ParseException e) {
             System.err.println(result);
             result_json = formatError();
         }
-        
-		return result_json;
-	}
-    
-    private JSONObject formatError() {
+
+        return result_json;
+    }
+
+    private JSONArray formatError() {
+        JSONArray response = new JSONArray();
         JSONObject error = new JSONObject();
         error.put("response", "error");
         error.put("type", "format error");
         error.put("content", "none-json response received from server");
-        return error;
+        response.add(error);
+        return response;
     }
-	
+
 }
