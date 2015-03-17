538c93e6dd542afd6319a148dae715039abdadc9
mhegedal <mhegedal@stud.ntnu.no>
2015-03-17 12:03:24 +0100

    eventMain

--- Kalender/src/controllere/EditEvent1Controller.java
+++ Kalender/src/controllere/EditEvent1Controller.java
@@ -17,7 +17,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import kalenderGUI.EditEventMain;
-import kalenderGUI.EventMain;
 import models.Group;
 import models.Person;
 import models.PersonComparator;
